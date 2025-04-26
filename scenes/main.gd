48dc3a00fe3d8b2a936907d281a06f783523e55b
haapasaari <helmi.haapasaari@tuni.fi>
2025-04-26 19:04:37 +0300

    loppusäätöä

--- scenes/main.gd
+++ scenes/main.gd
@@ -9,6 +9,10 @@ var game_started : bool = false
 var cells : int = 20
 var cell_size : int = 50
 
+var food_pos : Vector2
+var regen_food : bool = true
+
+
 var old_data : Array
 var snake_data : Array
 var snake : Array
@@ -26,12 +30,17 @@ var can_move: bool
 func _ready():
 	new_game()
 	
+#pelin aloitus
 func new_game():
+	get_tree().paused = false
+	get_tree().call_group("segments", "queue_free")
+	$GameOverMenu.hide()
 	score = 0
 	$Hud.get_node("Score").text = "SCORE: " + str(score)
 	move_direction = up
 	can_move = true
 	generate_snake()
+	move_food()
 	
 func generate_snake():
 	old_data.clear()
@@ -41,6 +50,7 @@ func generate_snake():
 	for i in range(3):
 		add_segment(start_pos + Vector2(0, i))
 		
+#madon luominen
 func add_segment(pos):
 	snake_data.append(pos)
 	var SnakeSegment = snake_scene.instantiate()
@@ -53,6 +63,7 @@ func add_segment(pos):
 func _process(delta):
 	move_snake()
 	
+#madon liikuttaminen
 func move_snake():
 	if can_move:
 		
@@ -84,7 +95,7 @@ func start_game():
 	game_started = true
 	$MoveTimer.start()
 
-
+#käärmeen/madon liikkuminen
 func _on_move_timer_timeout():
 	can_move = true
 	
@@ -100,14 +111,43 @@ func _on_move_timer_timeout():
 	check_self_eaten()
 	check_food_eaten()
 	
+#pelialueen ylittäminen
 func check_out_of_bounds():
-	if snake.data[0].x < 0 or snake_data[0].x > cells - 1 or snake_data[0].y > cells -1:
+	if snake_data[0].x < 0 or snake_data[0].x > cells - 1 or snake_data[0].y > cells -1:
 		end_game() 
-		
+
+#itseensä törmääminen
 func check_self_eaten():
 	for i in range(1, len(snake_data)):
 		if snake_data[0] == snake_data[i]:
 			end_game()
 			
+#marjojen syöminen
+func check_food_eaten():
+	if snake_data[0] == food_pos:
+		score += 1
+		$Hud.get_node("Score").text = "SCORE: " +str(score)
+		add_segment(old_data[-1])
+		move_food()
+			
+#marjojen spawnaaminen
+func move_food():
+	while regen_food:
+		regen_food = false
+		food_pos = Vector2(randi_range(0, cells -1), randi_range(0, cells - 1))
+		for i in snake_data:
+			if food_pos == i:
+				regen_food = true
+	$Food.position = (food_pos * cell_size) + Vector2(0, cell_size)
+	regen_food = true
+
+#pelin loppu
 func end_game(): 
-	pass
+	$GameOverMenu.show()
+	$MoveTimer.stop()
+	game_started = false
+	get_tree().paused = true
+
+#restart-napin toiminnot
+func _on_game_over_menu_restart():
+	new_game()
